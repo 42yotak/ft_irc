13e3ee74c8be1cef35899e09575eef3cccff2d09
maemi <dbsrl759@naver.com>
2023-01-11 00:43:04 +0900

    [fix] singleton to REAL SINGLETON

--- main.cpp
+++ main.cpp
@@ -8,15 +8,17 @@ int main(int argc, char *argv[]) {
 		std::cout << RED "./ircserv <port> <password>" NC "\n";
 	}
 
-	Server serv = Server(argv[1], argv[2]);
+	// Server serv = Server(argv[1], argv[2]);
 
 	try {
-		serv.on();
+		// serv.on();
+		Server::callServer(argv[1], argv[2]).on();
 	} catch(std::exception &e) {
 		std::cerr << e.what() << std::endl;
 		exit(1);
 	}
-	serv.off();
+	// serv.off();
+	Server::callServer().off();
 
 	return 0;
 }
