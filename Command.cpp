6bb773f027a0ba9893dcebab5257071cd57f9fec
42yotak <yotak@student.42seoul.kr>
2023-01-12 17:13:34 +0900

    [feat] welcomeprotocol

--- Command.cpp
+++ Command.cpp
@@ -14,35 +14,86 @@ void Command::welcomeProtocol(std::vector<std::string> cmds, Client *client) {
 	if (cmds[0] == "PASS") {
 		this->cmdPass(cmds, client);
 	} else if (cmds[0] == "USER") {
-		this->cmdPass(cmds, client);
+		this->cmdUser(cmds, client);
 	} else if (cmds[0] == "NICK") {
-		this->cmdPass(cmds, client);
+		this->cmdNick(cmds, client);
 	} else if (cmds[0] == "CAP") {
 		//ignore
 	} else {
 		//:irc.local 451 * KICK :You have not registered.
 	}
 
-	if (client->getIsRegistered() & (NICK | USER) == (NICK | USER)) {
+	if ((client->getIsRegistered() & (NICK | USER)) == (NICK | USER)) {
 		if (client->getIsRegistered() & PASS) {
 			// buf_write에 welcome message 쓰기
+			// 001 :<client> :Welcome to the <networkname> Network, <nick>
+			client->setBufWrite("001 :");
+			client->setBufWrite(client->getNickName());
+			client->setBufWrite(" :Welcome to the PokémonGo Network, ");
+			client->setBufWrite(client->getNickName());
+			client->setBufWrite("\r\n");
+			// 002 :<client> :Your host is <servername>, running version <version>
+			client->setBufWrite("002 :");
+			client->setBufWrite(client->getNickName());
+			client->setBufWrite(" :Your host is PokémonGo, running version 1.0\r\n");
+			// 003 :<client> :This server was created <datetime>
+			client->setBufWrite("003 :");
+			client->setBufWrite(client->getNickName());
+			client->setBufWrite(" :This server was created yesterday\r\n");
+			// 004 :<client> <servername> <version> <available user modes> <available channel modes>
+			client->setBufWrite("004 :");
+			client->setBufWrite(client->getNickName());
+			client->setBufWrite(" PokémonGo 1.0\r\n");
+			// 005 :<client> <1-13 tokens> :are supported by this server
+			client->setBufWrite("005 :");
+			client->setBufWrite(client->getNickName());
+			client->setBufWrite(" :are supported by this server\r\n");
 		} else {
-			// ERROR Closing link: (root@127.0.0.1) [Access denied by configuration]
+			// "<client> :Password incorrect"
+			client->setBufWrite(client->getNickName());
+			client->setBufWrite(" ");
+			client->setBufWrite(" :Password incorrect\r\n");
 			Server::callServer().removeClient(client->getFd());
 		}
 	}
 }
 
 void Command::cmdPass(std::vector<std::string> cmd, Client *client) {
+	if (cmd.size() < 2) {
+		//"<client> <command> :Not enough parameters"
+		//461 nickname PASS :Not enough parameters
+		client->setBufWrite("461 ");
+		client->setBufWrite(client->getNickName());
+		client->setBufWrite(" PASS");
+		client->setBufWrite(" :Not enough parameters\r\n");
+	}
 	if (cmd[1] == Server::callServer().getPassword())
 		client->setIsRegistered(PASS);
+	else
+		client->setIsRegistered(~PASS);
 }
 
 void Command::cmdNick(std::vector<std::string> cmd, Client *client) {
 // yuhwang
+	if (cmd.size() < 2) {
+		client->setBufWrite("461 :Not enough parameters\r\n");
+		return;
+	}
+	if (!this->isValidName(cmd[1])) {
+		client->setBufWrite("432 :");
+		client->setBufWrite(client->getNickName());
+		client->setBufWrite(" ");
+		client->setBufWrite(cmd[1]);
+		client->setBufWrite(" :Erroneus nickname\r\n");
+		return ;
+	}
+	client->setNickName(cmd[1]);
+	if (client->getIsRegistered() == REGISTER) {
+		
+	}
 }
 
-bool isValidName(const std::string& name) {
+bool Command::isValidName(const std::string& name) {
 	// https://modern.ircdocs.horse/#clients
 	if (name.find(' ') || name.find(',') || name.find('*') || \
 			name.find('?') || name.find('!') || name.find('@') || \
@@ -53,33 +104,38 @@ bool isValidName(const std::string& name) {
 }
 
 void Command::cmdUser(std::vector<std::string> cmd, Client *client) {
-	// USER root 0 * :root
-	// The minimum length of <username> is 1, ie. it MUST NOT be empty.
-	// isVerified 검사 완료시 client의 buffer에 write
+	// USER root * 0 :root
 	// USER [username] [nickname] [address] [:realname]
 	if (cmd.size() < 5) {
-		// Severname errono nickname errormsg
-		//:irc.local 461 * USER :Not enough parameters
+		// <command> :<reason>
+		// :irc.local 461 * USER :Not enough parameters
+		client->setBufWrite("461 :Not enough parameters\r\n");
 		return ;
 	}
 	if (client->getIsRegistered() & USER) {
-		// irc.your_domain_name
 		// :irc.local 462 * :You may not reregister
+		client->setBufWrite("462 :You may not reregister\r\n");
 		return ;
 	}
 	if (!isValidName(cmd[1])) {
-		// :irc.local 468 * USER :Your username is not valid
+		// :irc.local 468 * USER :Your username is not valid'
+		client->setBufWrite("468 :Your username is not valid\r\n");
 		return ;
 	}
 	client->setUserName(cmd[1]);
 	client->setRealName(cmd[4]);
 	client->setIsRegistered(USER);
+	if (client->getIsRegistered() == NOPASS) {
+		client->setBufWrite("ERROR :Closing Link: " + client->getNickName() + \
+			" [Access denied by configuration]\r\n");
+		// client 제거
+	}
 }
 void Command::cmdPing(std::vector<std::string> cmd, Client *client) {
 
 }
 void Command::cmdQuit(std::vector<std::string> cmd, Client *client) {
-
+	// QUIT [<message>]
 }
 
 void Command::cmdJoin(std::vector<std::string> cmd, Client *client) {
@@ -91,7 +147,14 @@ void Command::cmdPart(std::vector<std::string> cmd, Client *client) {
 
 }
 void Command::cmdKick(std::vector<std::string> cmd, Client *client) {
-
+	// KICK <channel> <nicks> [<reason>]
+	// ' ' split
+	// if (cmd < 2) -> error
+	// cmd[0] -> "channel"
+	// cmd[1] -> ',' split
+	// while(cmd[1][i]) -> "nickname"
+	// if(cmd[2])
+	// 	while(cmd[i]) -> "reason comment"
 }
 
 void Command::cmdNotice(std::vector<std::string> cmd, Client *client) {
--- Command.hpp
+++ Command.hpp
@@ -5,50 +5,6 @@
 
 class Client;
 
-/* PASS 461 462 464 */
-#define ERR_NEEDMOREPARAMS 461
-#define ERR_ALREADYREGISTERED 462
-#define ERR_PASSWDMISMATCH 464
-/* NICK 431 432 433 436 */
-#define ERR_NONICKNAMEGIVEN 431
-#define ERR_ERRONEUSNICKNAME 432
-#define ERR_NICKNAMEINUSE 433
-#define ERR_NICKCOLLISION 436
-/* USER 461 462 */
-/* PING 461 409 */
-#define ERR_NOORIGIN 409
-/* QUIT X */
-/* JOIN 461 403 405 475 474 473 476 332 333 353 366 */
-#define ERR_NEEDMOREPARAMS 461
-#define ERR_NOSUCHCHANNEL 403
-#define ERR_TOOMANYCHANNELS 405
-#define ERR_BADCHANNELKEY 475
-#define ERR_BANNEDFROMCHAN 474
-#define ERR_CHANNELISFULL 471
-#define ERR_INVITEONLYCHAN 473
-#define ERR_BADCHANMASK 476
-#define RPL_TOPIC 332
-#define RPL_TOPICWHOTIME 333
-#define RPL_NAMREPLY 353
-#define RPL_ENDOFNAMES 366
-/* PART 461 403 442 */
-#define ERR_NOTONCHANNEL 442
-/* KICK 461 403 482 441 442 */
-#define ERR_CHANOPRIVSNEEDED 482
-#define ERR_USERNOTINCHANNEL 441
-/* PRIVMSG 401 402 404 407 411 412 413 414 301 */
-#define ERR_NOSUCHNICK 401
-#define ERR_NOSUCHSERVER 402
-#define ERR_CANNOTSENDTOCHAN 404
-#define ERR_TOOMANYTARGETS 407
-#define ERR_NORECIPIENT 411
-#define ERR_NOTEXTTOSEND 412
-#define ERR_NOTOPLEVEL 413
-#define ERR_WILDTOPLEVEL 414
-#define RPL_AWAY 301
-
-#define ERR_NOPRIVILEGES 481
-
 class Command {
 	public:
 		Command();
@@ -67,4 +23,7 @@ class Command {
 
 		void cmdNotice(std::vector<std::string> cmd, Client *client);
 		void cmdPrivmsg(std::vector<std::string> cmd, Client *client);
+	
+	private:
+		bool isValidName(const std::string &name);
 };
